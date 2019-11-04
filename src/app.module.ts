79a6f8c06770c4128c42ea2ac8207a42c7dadfb6
Francisco Serrano <francisco.serrano372@gmail.com>
2019-11-03 23:57:30 -0300

    Adds authentication with JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { PhotoController } from './photo/controllers/photo.controller';
 import { PhotoService } from './photo/services/photo.service';
 import { PhotoAlbumController } from './photo/controllers/photoalbum.controller';
 import { PhotoAlbumService } from './photo/services/photoalbum.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     PhotoModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [
     PhotoController,
